80db693a12b684659fe442a3d842d9085d706480
Naveen Hegde <hegdenaveen1@gmail.com>
2017-12-18 19:37:33 +0530

    Update Binary Search.cpp

--- Binary Search.cpp
+++ Binary Search.cpp
@@ -6,16 +6,16 @@ int BinarySearchRecursive(vector<int> &vect,int low,int high,int key)
 {
     if(high<low)
         return -1;
-    int mid = floor(low  + (double)(high-low)/2);
+    int mid = low  + (high-low)/2;
     if(vect[mid]==key)
         return mid;
     else if(key<vect[mid])
     {
-        BinarySearch(vect,low,mid-1,key);
+        BinarySearchRecursive(vect,low,mid-1,key);
     }
     else
     {
-        BinarySearch(vect,mid+1,high,key);
+        BinarySearchRecursive(vect,mid+1,high,key);
     }
 }
 
@@ -23,7 +23,7 @@ int BinarySearchIterative(vector<int> &vect,int low,int high,int key)
 {
     while(low<=high)
     {
-        int mid = floor(low + (double)(high-low)/2);
+        int mid = low + (high-low)/2;
         if(vect[mid]==key)
             return mid;
         else if(key<mid)
