e57e3c368a6a0504ecf0b946da704fc8cbfe867b
Himanshu Gupta <himjan1416@gmail.com>
2017-12-18 17:08:33 +0530

    made the code cleaner

--- 3-way-quick-sort.cpp
+++ 3-way-quick-sort.cpp
@@ -1,4 +1,4 @@
-void partition(vector<int> a, int l, int r, int &i, int &j)
+void partition(vector<int> &a, int l, int r, int &i, int &j)
 {
     i = l-1, j = r;
     int p = l-1, q = r;
@@ -6,33 +6,21 @@ void partition(vector<int> a, int l, int r, int &i, int &j)
  
     while (true)
     {
-        // From left, find the first element greater than
-        // or equal to v. This loop will definitely terminate
-        // as v is last element
         while (a[++i] < v);
- 
-        // From right, find the first element smaller than or
-        // equal to v
+
         while (v < a[--j])
             if (j == l)
                 break;
  
-        // If i and j cross, then we are done
-        if (i >= j) break;
+        if (i >= j) 
+          break;
  
-        // Swap, so that smaller goes on left greater goes on right
         swap(a[i], a[j]);
- 
-        // Move all same left occurrence of pivot to beginning of
-        // array and keep count using p
         if (a[i] == v)
         {
             p++;
             swap(a[p], a[i]);
         }
- 
-        // Move all same right occurrence of pivot to end of array
-        // and keep count using q
         if (a[j] == v)
         {
             q--;
@@ -40,45 +28,23 @@ void partition(vector<int> a, int l, int r, int &i, int &j)
         }
     }
  
-    // Move pivot element to its correct index
     swap(a[i], a[r]);
- 
-    // Move all left same occurrences from beginning
-    // to adjacent to arr[i]
     j = i-1;
     for (int k = l; k < p; k++, j--)
         swap(a[k], a[j]);
- 
-    // Move all right same occurrences from end
-    // to adjacent to arr[i]
+    
     i = i+1;
     for (int k = r-1; k > q; k--, i++)
         swap(a[i], a[k]);
 }
- 
-// 3-way partition based quick sort
+
 void quicksort(vector<int> &a, int l, int r)
 {
-    if (r <= l) return;
+    if (r <= l) 
+      return;
  
     int i, j;
- 
-    // Note that i and j are passed as reference
     partition(a, l, r, i, j);
- 
-    // Recur
     quicksort(a, l, j);
     quicksort(a, i, r);
-}
-
-int main() 
-{
-  int n;
-  cin >> n;
-  vector<int> a(n);
-  for (size_t i = 0; i < a.size(); ++i) 
-  {cin >> a[i];}
-  quicksort(a, 0, a.size() - 1);
-  for (size_t i = 0; i < a.size(); ++i) 
-  {cout << a[i] << ' ';}
 }
