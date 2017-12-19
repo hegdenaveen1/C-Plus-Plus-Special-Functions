28805eea67f1dfed59620b8ef278be2c6bbd7cba
Naveen Hegde <hegdenaveen1@gmail.com>
2017-12-19 15:12:17 +0530

    Recursive and Iterative Linear Search

--- Linear Search.cpp
+++ Linear Search.cpp
@@ -1,7 +1,34 @@
-//Searches and Returns index of key in a vcector between given indexes inclusive,can be used for unsorted vector/// 
+//Searches and Returns index of key in a vcector between given indexes inclusive,can be used for unsorted vector 
 
 
 
+
+//Recursive Version (Subtract and Conquer)
+int LinearSearch(vector<int> &vect,int low,int high,int key)
+{
+    if(low>high)
+      return -1;
+    if(vect[low]==key)
+      return low;
+    LinearSearch(vect,low+1,high,key);
+}
+//Search bothways Recursive
+int LinearSearch(vector<int> &vect,int low,int high,int key)
+{
+    if(low>high)
+      return -1;
+    if(vect[low]==key)
+      return low;
+    if(vect[high]==key)
+        return high;
+    LinearSearch(vect,low+1,high-1,key);
+}
+
+
+
+
+
+//Iterative Version
 int LinearSearch(vector<int> &vect,int low,int high,int key)
 {
     for(int i=low;i<=high;i++)
@@ -11,3 +38,16 @@ int LinearSearch(vector<int> &vect,int low,int high,int key)
     }
     return -1;
 }
+//Search bothways Iterative
+int LinearSearch(vector<int> &vect,int low,int high,int key)
+{
+    for(int i=0;i<=high-low;i++)
+    {
+        if(vect[i+low]==key)
+            return i+low;
+        if(vect[high-i]==key)
+            return high-i;
+    }
+    return -1;
+}
+
