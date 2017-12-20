afc7b823cde11d03913f8f8718d06d2189cf8df7
Naveen Hegde <hegdenaveen1@gmail.com>
2017-12-20 21:40:37 +0530

    merged quicksort files

--- Quick Sort.cpp
+++ Quick Sort.cpp
@@ -4,6 +4,56 @@
 
 
 
+****************************************************************
+////////////////Quicksort 3 Partition///////////////////////////
+
+
+struct twopoint
+{
+  int left;
+  int right;
+};
+
+twopoint Partition(vector<int> &vec,int low,int high)
+{
+int pivot=vec[low];
+twopoint j;
+j.left=low;
+j.right=low;
+for(int i=low+1;i<=high;i++)
+{
+  if(vec[i]<pivot)
+  {
+    j.left++;
+    swap(vec[j.left],vec[i]);
+  }
+  if(vec[i]==pivot)
+  {
+    j.right++;
+    swap(vec[j.right],vec[i]);
+  }
+}
+swap(vec[j.left],vec[low]);
+return j;
+}
+
+
+void QuickSort(vector<int> &vect,int left,int right)
+{
+  if(left>=right)
+    return;
+  twopoint part = Partition(vect,left,right);
+  QuickSort(vect,left,part.left-1);
+  QuickSort(vect,part.right+1,right);
+
+}
+
+
+
+***************************************************************
+////////////////QuickSort Single Pivot///////////////////////// 
+
+
 int Partition(vector<int> &vec,int low,int high)
 {
 int pivot=vec[low];
@@ -31,3 +81,63 @@ void QuickSort(vector<int> &vect,int left,int right)
 
 }
 
+****************************************************************
+///////////////QuickSort Single Median Pivot////////////////////
+
+
+int medianlocation(vector<int> &vec,int a,int b,int c)
+{
+  if(vec[a]<=vec[b])
+  {
+    if(vec[b]<=vec[c])
+      return b;
+    if(vec[a]<=vec[c])
+      return c;
+    return a;
+  }
+  if(vec[c]<=vec[b])
+    return b;
+  if(vec[a]<=vec[c])
+    return a;
+  return c;
+}
+
+int Partition(vector<int> &vec,int low,int high)
+{
+int pivotlocation = medianlocation(vec,low,high,low+(high-low)/2);
+int pivot=vec[pivotlocation];
+int j=pivotlocation;
+for(int i=low;i<pivotlocation;i++)
+{
+  if(vec[i]>=pivot)
+  {
+    swap(vec[i],vec[pivotlocation]);
+    pivotlocation=i;
+    j=pivotlocation;
+  }
+}
+for(int i=pivotlocation+1;i<=high;i++)
+{
+  if(vec[i]<=pivot)
+  {
+    j++;
+    swap(vec[j],vec[i]);
+  }
+}
+
+swap(vec[j],vec[pivotlocation]);
+return j;
+}
+
+
+void QuickSort(vector<int> &vect,int left,int right)
+{
+  if(left>=right)
+    return;
+  int part = Partition(vect,left,right);
+  QuickSort(vect,left,part-1);
+  QuickSort(vect,part+1,right);
+
+}
+
+*************************************************************
