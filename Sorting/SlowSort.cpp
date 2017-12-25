fbf72d8faea78b0318bbfdac82ec0ddf62006ee2
Naveen Hegde <hegdenaveen1@gmail.com>
2017-12-25 23:32:00 +0530

    Update SlowSort.cpp

--- Sorting/SlowSort.cpp
+++ Sorting/SlowSort.cpp
@@ -1,5 +1,10 @@
-//Returns the sorted vector after performing SlowSort//
-//This algorithm works on multiply and surrender principle
+//Returns the sorted vector after performing SlowSort
+//It is a sorting algorithm that is of humorous nature and not useful. 
+//It's based on the principle of multiply and surrender, a tongue-in-cheek joke of divide and conquer. 
+//It was published in 1986 by Andrei Broder and Jorge Stolfi in their paper Pessimal Algorithms and Simplexity Analysis.
+//This algorithm multiplies a single problem into multiple subproblems
+//It is interesting because it is provably the least efficient sorting algorithm that can be built asymptotically, 
+//and with the restriction that such an algorithm, while being slow, must still all the time be working towards a result.
 
 
 
