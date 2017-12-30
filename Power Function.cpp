737fb90b5d2e59cb56c8d5d709121781e0782c62
Naveen Hegde <hegdenaveen1@gmail.com>
2017-12-30 15:55:59 +0530

    Iterative and Recursive Power

--- Power Function.cpp
+++ Power Function.cpp
@@ -1,9 +1,23 @@
 //Power function
 
+
+//Recursive Version
 double pow(double a, int n) 
 {
 if(n == 0) return 1;
 if(n == 1) return a;
 double t = pow(a, n/2);
 return t * t * pow(a, n%2);
+}
+
+//Iterative Version
+double pow(double a, int n) 
+{
+double ret = 1;
+while(n) 
+{
+if(n%2 == 1) ret *= a;
+a *= a; n /= 2;
+}
+return ret;
 }
