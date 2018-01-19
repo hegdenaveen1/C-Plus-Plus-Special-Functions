e0edca56db44ca3f2c264d8948eded7e04d8fcf2
Naveen Hegde <hegdenaveen1@gmail.com>
2018-01-19 19:11:37 +0530

    Matrix Operations

---
+++ Matrix operations.cpp
@@ -0,0 +1,19 @@
+//General Matrix Operations
+
+int a[1000][1000], b[1000][1000], c[1000][1000];
+
+void matrixmultiplication(int m,int p,int n)
+{
+	//returns the product of a m*p and p*n matrix
+	for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                c[i][j] = 0;
+                for (int k = 0; k < p; k++)
+                {
+                    c[i][j] = c[i][j] + a[i][k] * b[k][j];
+                }
+            }
+        }
+}
