18147717ebea9b6afaf5db2e190bcfcae03a9dd2
Naveen Hegde <hegdenaveen1@gmail.com>
2019-07-06 16:34:25 +0530

    Determinant of matrix of any size

--- Matrix operations.cpp
+++ Matrix operations.cpp
@@ -18,9 +18,55 @@ void matrixmultiplication(int m,int p,int n)
         }
 }
 
-int matrixdeterminent2x2()
+*****************Determinant of any matrix***********************************************
+
+vector< vector<int> > submatrix(vector<vector<int>> matrixA,int row,int column)
 {
-	return (a[1][1]*a[0][0])-(a[0][1]*a[1][0]);
+    vector< vector<int>> matrixB=matrixA;
+
+    if(matrixB.size()>row)
+    {
+        matrixB.erase(matrixB.begin()+row);
+    }
+    for(int i=0;i<matrixB.size();i++)
+    {
+        if(matrixB[i].size()>column)
+        {
+            matrixB[i].erase(matrixB[i].begin()+column);
+        }
+    }
+    return matrixB;
+}
+
+int matrixdeterminant(vector< vector<int> > matrixA)
+{   
+    int size=matrixA.size();
+    if(size==1)
+    {
+        return matrixA[0][0];
+    }
+    else if(size==2)
+    {
+        return (matrixA[1][1]*matrixA[0][0])-(matrixA[0][1]*matrixA[1][0]);
+    }
+    else
+        {
+            int ans=0;
+            for(int i=0;i<size;i++)
+            {
+
+                if(i%2)
+                {
+                    ans=ans-matrixA[0][i]*matrixdeterminant(submatrix(matrixA,0,i));
+                }
+                else
+                {
+                    ans=ans+matrixA[0][i]*matrixdeterminant(submatrix(matrixA,0,i));
+                }
+            }
+            return ans;
+        }
 }
 
 
+********************************************************************************
